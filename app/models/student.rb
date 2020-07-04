10638dd6958c096159378010c830f5e3dba3dcef
Megan Horsfield <meganthadev@gmail.com>
2020-07-04 23:48:21 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,6 +17,9 @@ class Student < ActiveRecord::Base
   def self.search(query)
     if query.present?
       where('NAME like ?', "%#{query}%")
-      
+    else
+      self.all
+    end
+  end  
   
 end
